eaf095cb706303bb9e207a7d7fc9339325c44112
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2021-12-27 11:33:02 +0100

    Initial commit

---
+++ Models/Blog.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBlogProject.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage ="The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage ="The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+        
+        public IFormFile Image { get; set; }
+
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string BlogId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage ="The {0} must be at least {2} and no more than {1} characters long")]
+        public string Title { get; set; }
+
+    }
+}
